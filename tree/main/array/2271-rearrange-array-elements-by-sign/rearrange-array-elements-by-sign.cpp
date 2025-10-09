5ef74e719d67c7925e32461ce603aaeec92da57f
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-09 16:40:50 +0530

    Time: 8 ms (51.48%) | Memory: 128.4 MB (57.48%) - LeetSync

--- tree/main/array/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ tree/main/array/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
 
-        vector<int> pos;
-        vector<int> neg;
-
+        int pos =0;
+        int neg = 1;
+        vector<int> ans(nums.size(), 0);
         for(auto num : nums){
             if(num/abs(num) == 1 ){ // positive
 
-                pos.push_back(num);
+                ans[pos] = num;
+                pos += 2;
 
             }
             if(num/abs(num) == -1 ){ // negative
 
-                neg.push_back(num);
+                ans[neg] = num;
+                neg += 2; //Skip to next odd index
 
             }
         }
 
-        vector<int> ans;
 
-        for(int i = 0; i<pos.size(); i++){
-
-
-            ans.push_back(pos[i]);  //begin with a positive integrer
         
-            ans.push_back(neg[i]);
-            
-        }
 
         return ans;
         
     }
 };
+
+
+// the positive and negative numbers have to be saved somewhere in my understanding and then interleaved
+// a vector is not better than a queue or a stack for storing those same sign integers
+// what if we store only one interger and freeball with the other??
+// push positives into the ans array directly and storw negatives
+// push positves only when the size of the answer array has been even? (makes sense as a negative number is added after each positive)
+// what if we encounter a lot of positive numbers without a negative hmmmmm (DEADEND HERE)
+
+// OH dumb shit, i can directly place all the positive integers at even indexes and negative at odd indexes
+// maintain two index pointers for that
