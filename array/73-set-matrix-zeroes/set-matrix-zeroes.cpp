423abbad136754244cddbd2ee948ecd8776fb1e8
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-10 19:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (51.74%) - LeetSync

--- array/73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ array/73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,31 +2,52 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
 
-    unordered_set<int> zerorow;
-        unordered_set<int> zerocol;
         int m = matrix.size();
         int n = matrix[0].size();
 
-        for(int i = 0;i < m; i++){
-            for(int j = 0 ; j<n ; j++){
+        bool flagifzeroexistsinrow = false;
+        bool flagifzeroexistsincol = false;
+
+        for(int i =0 ; i<m; i++){
+            if(matrix[i][0] == 0){
+                flagifzeroexistsinrow = true;
+            }
+        }
+
+        for(int i =0 ; i<n; i++){
+            if(matrix[0][i] == 0){
+                flagifzeroexistsincol = true;
+            }
+        }
+
+        for(int i = 0; i<m ; i++){
+            for(int j = 0; j<n ;j ++){
                 if(matrix[i][j] == 0){
-                    zerorow.insert(i);
-                    zerocol.insert(j);
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int k : zerorow){
-            for(int i = 0; i<n; i++){
-                matrix[k][i] =0;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
             }
         }
 
-        for(int n : zerocol){
-            for(int i = 0; i<m ; i++){
-                matrix[i][n] = 0; 
-           }
+        if(flagifzeroexistsinrow){
+            for(int i = 0;i<m;i++){
+                matrix[i][0] = 0;
+            }
         }
+
+        if(flagifzeroexistsincol){
+            for(int i = 0;i<n;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        
     }
 };
 
@@ -46,4 +67,6 @@ public:
 
 // Instead of storing vectors i could just note which row and whihc column have zeroes
 //  that would lessen it down to O(m+n)
-// 
+
+
+// if i,j found 0 make the first row/column = 0, then scan it again andif 0 found in a row/column in the first iteration then zero the entire row/column
