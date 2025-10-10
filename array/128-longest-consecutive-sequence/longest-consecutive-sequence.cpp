94ca40ced72ca1b4d3106778ffb0cb9aec36d8df
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-10 18:22:29 +0530

    Time: 19 ms (81.66%) | Memory: 60.1 MB (93.41%) - LeetSync

---
+++ array/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        if(nums.size() == 0) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int longest = 1;
+        int counter = 1;
+
+        for(int i =1 ; i <nums.size(); i++){
+            if(nums[i] == nums[i-1]) continue;
+            if(nums[i] == nums[i-1] + 1) counter++;
+            else{
+                counter = 1;
+            }
+            
+                longest = max(longest, counter);
+        } 
+        return longest;
+    }
+};
+
+// longest subsequence required as in 1,2,3,4 or 76,77, 78
+// sorting takes O(1)
+// then apply two pointer?
+// maintain a longest counter
