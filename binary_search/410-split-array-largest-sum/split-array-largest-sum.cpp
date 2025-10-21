b58cf987e5b11de7383207cb9599e23289f3c239
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-21 22:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (57.73%) - LeetSync

--- binary_search/410-split-array-largest-sum/split-array-largest-sum.cpp
+++ binary_search/410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
-    int isPossible(vector<int>& arr, int mid) {
-        int count = 1;
-        long long CurrentSum = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] + CurrentSum <= mid) {
-                CurrentSum += arr[i];
-            } else {
-                count++;
-                CurrentSum = arr[i];
+
+    bool isPossible(vector<int>& nums, int k, int limit){
+ int sum = 0;
+            int splits = 1;
+
+            // need to assign at max limit integers to a split, if i can do it in less than k splits then its good otherwise no
+           
+            for(int n : nums){
+                if(n > limit) return false;
+                if(sum + n > limit){
+                    sum = n;
+                    splits++; //start a new split
+                }
+                else{
+                    sum += n;
+                }
             }
-        }
-        return count;
+
+
+        
+                    return splits <= k;
     }
+
+    
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end(), 0);
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isPossible(nums, mid) > k) {
-                low = mid + 1;
-            } else {
+
+        
+        int maxi = INT_MIN;
+        for(int n : nums){
+            maxi = max(n, maxi);
+        }
+        int low = maxi;
+        int high = accumulate(nums.begin(), nums.end(), 0LL);
+
+        int mid;
+
+        while(low<= high){
+
+            mid = (low +high)/2;
+            if(isPossible(nums, k , mid)) //if possible then lets try to go minimise it even more
+            {
                 high = mid - 1;
+
             }
+            else low = mid +1 ;
         }
         return low;
+       
+        
     }
+    
 };
