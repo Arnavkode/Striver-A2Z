da517b92fa962fe579f556fa4a43e162155d75be
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-23 20:20:33 +0530

    Time: 2 ms (56.04%) | Memory: 10 MB (11.17%) - LeetSync

--- binary_search/242-valid-anagram/valid-anagram.cpp
+++ binary_search/242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s == t);
+
+        if(s.size() != t.size()) return false;
+        unordered_map<char, int> map1;
+        unordered_map<char, int> map2;
+
+        for(int i =0;i<s.size(); i++){
+            map1[s[i]]++;
+            map2[t[i]]++;
+        }
+
+        
+
+        for(auto p : map1){
+            if(map1[p.first] != map2[p.first]) return false;
+        }
+        return true;
     }
 };
 
 // Anagram = word derived by jumbling another word
+// sort technique is O(nlogn)
+// could use two maps
