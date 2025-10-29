cfef3427f831f45153b88e5688dc200e59116f80
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-29 17:05:49 +0530

    Time: 8 ms (79.90%) | Memory: 11.8 MB (52.83%) - LeetSync

--- strings/141-linked-list-cycle/linked-list-cycle.cpp
+++ strings/141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,20 +11,29 @@ public:
     bool hasCycle(ListNode *head) {
 
         if(head == nullptr) return false;
-        ListNode* curr = head;
+        if(head->next == nullptr) return false;
+        ListNode* tort = head;
+        ListNode* hare = head;
 
-        // could get a map of addresses
-        unordered_map<ListNode*, int> mp;
-
-        while(curr->next != nullptr && mp.count(curr -> next) == 0){
-            mp[curr]++;
-            curr = curr -> next;
-            
+        while(hare != nullptr && hare -> next!= nullptr){
+            hare = hare->next->next;
+            tort = tort->next;
+            if(hare == tort) return true;
         }
-        if (curr-> next == nullptr) return false;
-        else return true;
+
+        return false;
+
         
     }
 };
 
 // acc to example the cycle could easily be searched with the help of if theres no null ptr
+// the first approach was to maintain a hashp mao and if i see a node again il lstop traversing
+// if the final curr ptr after traversing is pointing to null, the i  have no loop
+// but if it points to smthg else then i dont have a loop
+// but this bad soln as per tc and sc
+
+// if i had a length parameter, we could have had timed loop to look for nullptr
+
+// OKAY I saw TWO pointer in a soln title, major help 
+// tortoise hair hi hai bc
