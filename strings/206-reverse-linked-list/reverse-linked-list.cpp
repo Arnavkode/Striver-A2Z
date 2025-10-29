538b400a1dcb5c4586cfe23eb55161c937b87ebc
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-10-29 15:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (9.58%) - LeetSync

--- strings/206-reverse-linked-list/reverse-linked-list.cpp
+++ strings/206-reverse-linked-list/reverse-linked-list.cpp
@@ -9,23 +9,43 @@
  * };
  */
 class Solution {
+
+
 public:
+
+    void reverseLL(ListNode* node, ListNode* ptr){
+        if(node -> next != nullptr){
+            reverseLL(node->next, node);
+        }
+
+        node->next = ptr;
+        return;
+        
+    }
     ListNode* reverseList(ListNode* head) {
 
         ListNode* curr = head;
-        ListNode* prev = nullptr;
-        ListNode* nexttemp;
+        // ListNode* prev = nullptr;
+        // ListNode* nexttemp;
 
         // maintain next prev and current
 
-        while(curr != nullptr){
-            nexttemp = curr->next;
-            curr-> next = prev;
-            prev= curr;
-            curr = nexttemp;
-        }
+        // while(curr != nullptr){
+        //     nexttemp = curr->next;
+        //     curr-> next = prev;
+        //     prev= curr;
+        //     curr = nexttemp;
+        // }
 
-        return prev;
+        if(head == nullptr) return nullptr;
+
+        while(curr->next != nullptr){
+            curr = curr -> next;
+        }
+        reverseLL(head, nullptr);
+
+
+        return curr;
 
         
         
@@ -35,3 +55,6 @@ public:
 // need to reverse the linked list
 // go to last
 // make it point to prev ig. lets try
+
+// to do it recursively is okay. i can think of a function
+// that would take in a node and terminate on if the next to that node is a null ptr
