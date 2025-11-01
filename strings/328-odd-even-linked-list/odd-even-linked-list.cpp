8fd2a732145bf52e708e525f2cc00a855c1eb973
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-11-01 19:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (63.33%) - LeetSync

--- strings/328-odd-even-linked-list/odd-even-linked-list.cpp
+++ strings/328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -12,36 +12,49 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
 
-        ListNode* curr = head;
-        int counter = 1;
-        queue<int> odd;
-        queue<int> even;
-        while(curr != nullptr){
-            if(counter%2 == 1){
-                odd.push(curr->val);
-            }
-            if(counter%2 == 0){
-                even.push(curr->val);
-            }
-            curr = curr->next;
-            counter++;
-        }
         
-        curr = head;
+       
+       if(head == nullptr) return nullptr;
+        if(!head->next) return head;
+
+       ListNode* oddHead = head;
+       ListNode* evenHead = head->next;
+       ListNode* curr = evenHead->next;
+       
+       
+       ListNode* oddTail  = oddHead;
+       ListNode * evenTail = evenHead;
+
+        long long counter = 1;
 
         while(curr != nullptr){
-            if(!odd.empty()){
-                curr->val = odd.front();
-                odd.pop();
-            }
-            else{
-                curr->val = even.front();
-                even.pop();
-            }
-            curr = curr->next;
+        if(counter %2 == 1){
+            oddTail -> next = curr;
+            oddTail = oddTail->next;
         }
-        return head;
+        else{
+            evenTail -> next = curr;
+        evenTail = evenTail -> next;
+        }
+        curr = curr->next;
+        counter++;
+       }
+        evenTail -> next = nullptr;
+       oddTail -> next = evenHead;
+
+       return oddHead;
+       
+
+
+       
+
     }
 };
 
-// primary intuition goes that store and chenge values of even and odd respectively
+// primary intuition goes that store and chenge indices of even and odd respectively
+
+// but this intuition retarded, kinda
+// lets have two heads, one odd and one even
+// now traversing the LL, if i find odd, lets connect the oddtail to that and if even then even to that
+// at the end lets connect the odd tail to even tail
+
