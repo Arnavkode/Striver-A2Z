1457bd91c25ffb87e23ae7b3d34e533b5447280e
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-11-02 16:44:54 +0530

    Time: 40 ms (38.39%) | Memory: 18.4 MB (83.00%) - LeetSync

--- strings/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ strings/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,23 +8,48 @@
  */
 class Solution {
 public:
+   
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-    
-    unordered_map<ListNode*, int> freq;
-    ListNode* curr = headA;
-    while(curr){
-        freq[curr]++;
-        curr = curr -> next;
-    }
-    curr = headB;
-    while(curr){
-        if(freq.count(curr) == 1) break;
-        else curr = curr -> next;
-    }
 
-    return curr;
-        
+        if(headA == nullptr || headB == nullptr) return nullptr;
+        int L1 = 1;
+        int L2 = 1;
+
+        ListNode* curr1 = headA;
+        ListNode* curr2 = headB;
+
+        while(curr1){
+            L1++;
+            curr1 = curr1 -> next;
+        }
+        while(curr2){
+            L2++;
+            curr2 = curr2 -> next;
+        }
+
+        int diff = abs(L2-L1);
+        curr1 = headA;
+        curr2 = headB;
+        if(L1 > L2){
+            while(diff--) curr1 = curr1-> next;
+
+            while(curr1){
+                if(curr1 == curr2) break;
+                curr1 = curr1  ->next;
+                curr2 = curr2 -> next;
+            }
+        }else{
+            while(diff--) curr2 = curr2 -> next;
+
+            while(curr2){
+                if(curr1 == curr2) break;
+                curr1 = curr1  ->next;
+                curr2 = curr2 -> next;
+            }
+        }
+
+        return curr1;
     }
 };
 
@@ -33,4 +58,11 @@ public:
 // a bad idea for longer lists
 
 // maintaining two pointers but for?
-// could use a frequency map for pointers, tbh shoud implement that
+// could use a frequency map for pointers, tbh shoud implement that, not the best solution of course
+// TC: O(N+M) SC: (N+M)
+
+// what if i make a dummy node point to both of A and B ke head by making a whole ass new class lyasgo
+
+
+// Best approach is to have the two pointers be at equal distance sfrom the end and then traversed until they meet
+// 
