ab852d45df0ebaa9ecd020339f5cb3573bdf3238
Arnav Gupta <arnavgupta.infinity@gmail.com>
2025-11-14 14:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (27.84%) - LeetSync

---
+++ Recursion/90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void helper(vector<int>& nums, vector<int>& sub, int iter, vector<vector<int>>& ans){
+        if(iter == nums.size()){
+            ans.push_back(sub);
+            return;
+        }
+        sub.push_back(nums[iter]);
+        helper(nums, sub, iter + 1, ans);
+        sub.pop_back();
+
+        int next_iter = iter + 1;
+        while(next_iter < nums.size() && nums[next_iter] == nums[iter]){
+            next_iter++;
+        }
+        helper(nums, sub, next_iter, ans);
+        return;
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        vector<int> sub;
+sort(nums.begin(), nums.end());
+        helper(nums, sub, 0, ans);
+       sort(ans.begin(), ans.end());
+        return ans;
+
+
+        
+    }
+};
+
+// the decision just pick one up and dont with the other
+// must not contain duplicates??
+// ill need to find if the subset has been done or no?
+// could do a whole ass check after getting the ans once
