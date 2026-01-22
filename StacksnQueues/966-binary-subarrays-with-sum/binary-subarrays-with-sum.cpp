444d9f410dfbe77d2752167bd0a39e54a48c2496
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-01-22 15:33:44 +0530

    Time: 1 ms (61.96%) | Memory: 32.6 MB (81.16%) - LeetSync

--- StacksnQueues/966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ StacksnQueues/966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+
+    int helper(vector<int> & nums, int goal){
+        if(goal<0) return 0;
+        int left = 0;
+        int currsum =0;
+        int ans =0 ;
+
+        for(int right = 0; right < nums.size() ; right++){
+            currsum += nums[right];
+
+            while(currsum > goal){
+                currsum = currsum - nums[left];
+                left++;
+            }
+
+            ans += (right - left +1);
+        }
+        return ans;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         // kinda like two sum
 
@@ -7,21 +26,7 @@ public:
         // i think its better to use prefix sum when dealing with problems where i have a sum and i have to 
         // find ALL ARRAYS
 
-        int left = 0;
-        int currsum = 0;
-        int ans =0;
-        unordered_map<int,int> prefix; //prefix seum against its frequency
-        prefix[0] = 1;
-        for(int n : nums){
-            currsum += n;
-            
-            if(prefix.count(currsum - goal)){
-                ans += prefix[currsum - goal];
-            }
-            prefix[currsum]++;
-        }
-
-        return ans;
+        return helper(nums, goal) - helper(nums, goal - 1);
 
         
     }
@@ -29,3 +34,10 @@ public:
 
 // i have a binary array and a target
 // need to return all non empty arrays with that sum
+
+// I DONT UNDERSTAND TS
+
+// but this approach has a time complexity of O(N)
+
+// a better approach seen was, that subtract the number of arrays that have sum at most n
+// from the number of arrays that have sum at most n-1
