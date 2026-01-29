f837b733e0ebb98065317905730117a7d8c2b567
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-01-29 17:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.16%) - LeetSync

--- heaps_pq/45-jump-game-ii/jump-game-ii.cpp
+++ heaps_pq/45-jump-game-ii/jump-game-ii.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
 
-        int ans = 0;
-        int minjump = 0;
+        int farthest = 0;
         int n = nums.size();
-        vector<int> dp(n, INT_MAX);
-        // dp[i] gives the minimum jumps required to reach i
+        int currentend = 0;
+        int jumps = 0;
 
-        dp[0] = 0;
-        // i think i also have to maintian the maxreach
-        int maxreach = 0;
-        for(int i = 0  ; i < n; i++){
-            //okay so i need to check for each 
+        for(int i = 0; i< n-1; i++){
+            farthest = max(farthest, i + nums[i]);
 
-            for(int j = 1; j <= nums[i] && i+j< n; j++){
-                dp[i+j] = min(dp[i+j], dp[i] + 1);
-            }
-            
+            if(i == currentend){ jumps++; currentend = farthest;};
+
         }
-
-        return dp[n-1];
-
-        return ans;
+        return jumps;
         
     }
 };
@@ -31,3 +22,7 @@ public:
 // need to check how many times is maxreach changed
 
 // need to find the sortest path to raech a certain index
+// uhm okay so dp solution is v slow
+
+// i heard a greedy bfs
+// so pruning a tree, if it reaches the jumpcount more than the current min
