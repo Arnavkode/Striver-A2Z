fa68db8383526d6fed820b9d8eed84c2736deb23
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-01-29 15:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (21.23%) - LeetSync

--- heaps_pq/55-jump-game/jump-game.cpp
+++ heaps_pq/55-jump-game/jump-game.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        // will check at each stage, all the options that we might have
+
         int maxReach = 0;
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i++) {
-            // FAILURE CASE:
-            // If the current index 'i' is beyond our maximum reach,
-            // it means we are stuck and cannot step here.
+        int n  = nums.size();
+
+        for(int i = 0; i< n ; i++){
             if(i > maxReach) return false;
-            
-            // GREEDY UPDATE:
-            // Can we extend our reach from this position?
-            // maxReach = max(current best, current index + jump power)
-            maxReach = max(maxReach, i + nums[i]);
-            
-            // Optimization: If we can already reach the end, stop early.
-            if(maxReach >= n - 1) return true;
+
+            maxReach = max(maxReach, i + nums[i]); 
+
+            if(maxReach >= n )return true;
         }
-        
+
         return true;
+
     }
 };
+
+// okay jump game
+// basically at each step i need to check which jump would make the most sense
