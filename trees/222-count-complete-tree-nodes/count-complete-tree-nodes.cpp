bb1f1ffd94769acfbe8670248afe89f4db952c69
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-02-12 15:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (72.26%) - LeetSync

---
+++ trees/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int last = 0;
+int level = 0;
+
+    int countNodes(TreeNode* root) {
+
+        if(!root) return 0;
+
+        int hl=0, hr=0;
+
+        TreeNode *l=root, *r=root;
+
+        while(l) {hl++;l=l->left;}
+
+        while(r) {hr++;r=r->right;}
+
+        if(hl==hr) return pow(2,hl)-1;
+
+        return 1+countNodes(root->left)+countNodes(root->right);
+
+    }
+    
+};
+// normal traversal would give O(n)
+// i just need to go to the last level;
