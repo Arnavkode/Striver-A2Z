5e580b0e75ef0b636ddeea27887dd34dcfc16600
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-02-23 14:11:54 +0530

    Time: 528 ms (30.97%) | Memory: 123.7 MB (37.86%) - LeetSync

---
+++ trees/1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+
+        unordered_set<string> seen;
+        deque<char> q;
+
+        for(char c : s){
+            q.push_back(c);
+            if(q.size() == k){
+                seen.insert({q.begin(), q.end()});
+                q.pop_front();
+                // a sliding window is maintained of k characters
+                // as soon as we reach that limit we just push our current substr
+                // for checking/inserting in the set
+            }
+        }
+
+
+        if(seen.size() >= pow(2,k)) return true;
+        else return false;
+
+
+        
+    }
+};
+
+// brutest force would be to have every possible substr
+// then compare
+// the best solution seems like to store every possible combination in the set 
+// and then if the size is less than or equal to 2^k then we have our result
