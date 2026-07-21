543c014d23252d122e4b545ecffcfca5fe56c954
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-07-21 17:23:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (11.47%) - LeetSync

--- trees/242-valid-anagram/valid-anagram.cpp
+++ trees/242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        
+        unordered_map<char,int> freq1;
+        unordered_map<char,int> freq2;
 
-        return s == t;
+        for(char c : s) freq1[c]++;
+        for(char c : t) freq2[c]++;
+
+        return freq1 == freq2;
     }
 };
