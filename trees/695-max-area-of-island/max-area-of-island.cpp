0d9e7bc372ec49b7c24f01a3879869b5f4ae8657
Arnav Gupta <arnavgupta.infinity@gmail.com>
2026-07-30 17:03:38 +0530

    Time: 27 ms (5.14%) | Memory: 39 MB (5.03%) - LeetSync

---
+++ trees/695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,49 @@
+class Solution {
+
+public:
+
+    vector<vector<int>> dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    int dfs(int srx , int sry , vector<vector<int>>& grid, vector<vector<int>> &vis){
+
+        stack<pair<int,int>> stk;
+        stk.push({srx,sry});
+        vis[srx][sry] = 1;
+        int area  = 1;
+        while(!stk.empty()){
+            int x = stk.top().first;
+            int y = stk.top().second;
+            stk.pop();
+            for(auto v : dirs){
+                int newx = x + v[0];
+                int newy = y + v[1];
+
+                if(newx >= 0 && newx < grid.size() && newy >= 0 && newy < grid[0].size() && grid[newx][newy] == 1 && vis[newx][newy] == 0){
+                    vis[newx][newy] = 1;
+                    area++;
+                    stk.push({newx, newy});
+                }
+            }
+             
+        }
+        return area;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+ vector<vector<int>> vis(m,  vector<int> (n));
+    int ans  =0 ;
+
+        for(int i =0 ; i < grid.size() ; i++){
+            for(int j =0 ; j < grid[0].size() ; j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                    
+
+                    ans = max(ans, dfs(i, j, grid, vis));
+                }
+            }
+        }
+
+        return ans;
+    }
+};
